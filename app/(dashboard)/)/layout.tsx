58cc44b21e2b193419817f613fe6ae5437eba17e
sanskarcodespace <sanskar.sharma.tech@gmail.com>
2026-04-03 13:18:32 +0530

    chore: remove all application routes and page components

--- app/(dashboard)/)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { SidebarLayout } from "@/components/layout/SidebarLayout";
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <SidebarLayout>{children}</SidebarLayout>;
-}
